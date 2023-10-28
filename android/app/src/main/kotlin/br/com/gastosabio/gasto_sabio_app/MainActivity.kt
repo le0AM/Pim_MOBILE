c6ac75d9ce5713c51331dc92816284e955927bc6
Leonardo Amorim <leo.amorimsouza@hotmail.com>
2023-10-28 14:03:58 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/br/com/gastosabio/gasto_sabio_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gastosabio.gasto_sabio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/release/br/com/gastosabio/gasto_sabio_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.gastosabio.gasto_sabio_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "br.com.gastosabio.gasto_sabio_app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "1.5";
+}
